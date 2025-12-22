4121dc6c8fb1fe5263d9ea420786a41e4f09a7d1
moriwx <64824605+moriwx@users.noreply.github.com>
2025-12-23 01:14:17 +0800

    Regular updates

--- scripts/partwkpics.py
+++ scripts/partwkpics.py
@@ -8,11 +8,12 @@ def crop_to_inscribed_circle(input_path, output_path):
     width, height = img.size
     mask = Image.new("L", (width, height), 0)
     draw = ImageDraw.Draw(mask)
-    radius = min(width, height) // 2
+    radiusw = width // 2
+    radiush = height // 2
     center = (width//2, height//2)
     draw.ellipse(
-        (center[0]-radius, center[1]-radius, 
-         center[0]+radius, center[1]+radius),
+        (center[0]-radiusw, center[1]-radiush, 
+         center[0]+radiusw, center[1]+radiush),
         fill=255
     )
     img.putalpha(mask)
@@ -68,39 +69,57 @@ def generate_color_image(colors, outputfile, offseta=0, offsetb=0):
             pixels[x, y] = color
     img.save(outputfile)
 
-input_path = 'E://Pictures//ACGN//PJSK//Qvbs//'
+# input_path = 'E://Pictures//ACGN//PJSK//Q25//'
+input_path = 'E://Pictures//ACGN//profiles//'
 image_extension = '.png'
-image_size = (400, 400) # (128, 128)
-output_path = 'E://Videos//nicokara//250603kousen//'
-char_zh = '初音未来；小豆泽心羽；白石杏；东云彰人；青柳冬弥'.split('；')
-char_en = ['miku', 'khn', 'an', 'akt', 'toya']
-# char_all_en = ['miku', 'len', 'mei', 'khn', 'an', 'akt', 'toya', 'knd', 'mfy', 'ena', 'mzk']
-color_rgb = {'miku':(51, 204, 187), 'len':(255,238,17), 'mei':(221,68,68),
-    'khn':(255, 102, 153), 'an':(0, 187, 221), 'akt':(255, 119, 34), 'toya':(0, 119, 221),
-    'knd':(187,102,136), 'mfy':(136,136,204), 'ena':(204,170,136), 'mzk':(221,170,204)}
+image_size = (256, 256) # (400, 400) # (128, 128)
+output_path = 'E://Videos//nicokara//251222starmine//'
+# char_zh = 'KAITO；星乃一歌；天马咲希；望月穗波；日野森志步'.split('；')
+# char_zh = ['リン', '奏', 'まふゆ', '絵名', '瑞希']
+# char_en = ['rin', 'knd', 'mfy', 'ena', 'mzk']
+char_zh = ['佑芽', '美鈴', '星南']
+char_en = ['hume', 'hmsz', 'jsna']
+# char_en = ['KAFU', 'SEKAI', 'RIME', 'COKO', 'HARU']
+# char_en = ['KAF', 'KAFU', 'ISEKAI', 'SEKAI', 'RIM', 'RIME', 'KOKO', 'COKO', 'HARUS', 'HARU']
+char_all_en = ['miku', 'rin', 'len', 'luka', 'mei', 'kai', 'ick', 'saki', 'hnm', 'shiho', 'mnr', 'hrk', 'airi', 'szk',
+               'khn', 'an', 'akt', 'toya', 'tks', 'emu', 'nene', 'rui', 'knd', 'mfy', 'ena', 'mzk']
+char_all_ja = ['ミク', 'リン', 'レン', 'ルカ', 'MEIKO', 'KAITO', '一歌', '咲希', '穂波', '志歩', 'みのり', '遥', '愛莉', '雫',
+               'こはね', '杏', '彰人', '冬弥', '司', 'えむ', '寧々', '類', '奏', 'まふゆ', '絵名', '瑞希']
+char_all_zh = ['ミク', 'リン', 'レン', 'ルカ', 'MEIKO', 'KAITO', '星乃一歌', '天马咲希', '望月穗波', '日野森志步', 'みのり', '遥', '愛莉', '雫',
+               'こはね', '杏', '彰人', '冬弥', '司', 'えむ', '寧々', '類', 'knd', 'mfy', 'ena', 'mzk']
+# color_rgb = {'miku':(51, 204, 187), 'len':(255,238,17), 'mei':(221,68,68),
+#     'khn':(255, 102, 153), 'an':(0, 187, 221), 'akt':(255, 119, 34), 'toya':(0, 119, 221),
+#     'knd':(187,102,136), 'mfy':(136,136,204), 'ena':(204,170,136), 'mzk':(221,170,204)}
 main_fontsize = 90
 zoom_scale = 80
 
-if __name__=='__main__':
-    for length in range(len(char_en)):
-        # if length in (2,3): continue
-        for combo in itertools.combinations(range(len(char_en)), length+1):
-            images = [Image.open(input_path+f"{char_en[i]}{image_extension}").convert("RGBA") 
-                     for i in combo]
+speakers = 'KAITO；星乃一歌；天马咲希；望月穗波；日野森志步；合唱；KAITO、星乃一歌；天马咲希、望月穗波、日野森志步；KAITO、星乃一歌、天马咲希、望月穗波、日野森志步'.replace('、',';').split('；')
+speakers = ['リン;絵名', 'リン;まふゆ', 'リン;奏;瑞希', 'まふゆ;絵名;瑞希', 'リン;奏;まふゆ;絵名', 'リン;奏;まふゆ;瑞希', 'リン;奏', 'リン;まふゆ;瑞希', '奏;絵名;瑞希',
+'リン;奏;絵名;瑞希', 'リン;瑞希', '奏', '絵名', '瑞希', 'まふゆ', 'リン']
 
-            canvas = Image.new('RGBA', (image_size[0] * len(combo), image_size[1]), (0, 0, 0, 0))
-            
-            x_offset = 0
-            for img in images:
-                canvas.paste(img, (x_offset, 0), mask=img)
-                x_offset += img.width
-            
-            filename = f"{''.join([char_en[i] for i in combo])}.png"
-            canvas.save(output_path+filename)
-            # if length == 0:
-            #     generate_color_image([color_rgb[char_en[i]] for i in combo], output_path+'color//'+filename, 0, -10)
-            # print(f"@Emoji=【{';'.join([char_zh[i] for i in combo])}】,{filename},,NoDecor,MarginRight=15,MarginBottom=15")
-            print(f"@Emoji=【{';'.join([char_zh[i] for i in combo])}】,{filename},,Zoom={zoom_scale}%,NoDecor"+ \
-                f",MarginRight=-999,MarginBottom={-main_fontsize*zoom_scale/100-2}") # canvas.width/canvas.height*98*zoom_scale/100
-    # print(f"@Emoji=【合唱】,{filename},,Zoom={zoom_scale}%,NoDecor,MarginRight=-999,MarginBottom={-main_fontsize*zoom_scale/100}")
-    # crop_to_inscribed_circle(output_path+'introv.jpg', output_path+'introv.png')
+if __name__=='__main__':
+    # for i in ['KAF', 'ISEKAI', 'RIM', 'KOKO', 'HARUS']:
+    #     crop_to_inscribed_circle(output_path+i+'.jpg', output_path+i+'.png')
+    for length in range(len(char_en)):
+        # if length in (1,2,3): continue
+        for combo in itertools.combinations(range(len(char_en)), length+1):
+            if 1: # ';'.join([char_zh[i] for i in combo]) in speakers: # 1
+                images = [Image.open(input_path+f"{char_en[i]}{image_extension}").convert("RGBA").resize(image_size)
+                         for i in combo]
+
+                canvas = Image.new('RGBA', (image_size[0] * len(combo), image_size[1]), (0, 0, 0, 0))
+                
+                x_offset = 0
+                for img in images:
+                    canvas.paste(img, (x_offset, 0), mask=img)
+                    x_offset += img.width
+                
+                filename = f"{''.join([char_en[i] for i in combo])}.png"
+                canvas.save(output_path+filename) # 角色拼图
+                # if length == 0:
+                #     generate_color_image([color_rgb[char_en[i]] for i in combo], output_path+'color//'+filename, 0, -10) # 单色图
+                # print(f"@Emoji=【{';'.join([char_zh[i] for i in combo])}】,{filename},,NoDecor,MarginRight=15,MarginBottom=15") # 中插
+                print(f"@Emoji=【{';'.join([char_zh[i] for i in combo])}】,{filename},,Zoom={zoom_scale}%,NoDecor"+ \
+                    f",MarginRight=-999,MarginBottom={-main_fontsize*zoom_scale/100-2}") # canvas.width/canvas.height*98*zoom_scale/100 # 下插
+    print(f"@Emoji=【合唱】,{filename},,Zoom={zoom_scale}%,NoDecor,MarginRight=-999,MarginBottom={-main_fontsize*zoom_scale/100-2}")
+    # crop_to_inscribed_circle(output_path+'channels4_profile.jpg', output_path+'2.png') # 裁圆
